19e255ef21aa52f70f0e91e1cb27a4fffe4d35bd
yoosuf-mk <yoosuf.mohamedkamal@ontariotechu.net>
2024-03-07 18:56:54 -0500

    Resource/js

--- SpamDetectorClient/js/main.js
+++ SpamDetectorClient/js/main.js
@@ -9,3 +9,66 @@ function closeNav() {
   document.getElementById("main").style.marginLeft= "0";
 }
 
+// Function to handle errors in fetch requests
+function handleFetchError(error) {
+  console.error('Error fetching data:', error);
+}
+// Function to add a row to the table
+function addRow(testFile) {
+  let table = document.getElementById('chart').getElementsByTagName('tbody')[0];
+  let newRow = table.insertRow();
+
+  let filenameCell = newRow.insertCell(0);
+  let spamProbabilityCell = newRow.insertCell(1);
+  let classCell = newRow.insertCell(2);
+
+  filenameCell.textContent = testFile.filename;
+  spamProbabilityCell.textContent = testFile.spamProbability;
+  classCell.textContent = testFile.actualClass;
+}
+
+// Function to update accuracy value in the UI
+function updateAccuracy() {
+  fetch('http://localhost:8080/spamDetector-1.0/api/spam/accuracy', {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('accuracy').setAttribute('value', data.toString());
+    })
+    .catch(handleFetchError);
+}
+
+// Function to update precision value in the UI
+function updatePrecision() {
+  fetch('http://localhost:8080/spamDetector-1.0/api/spam/precision', {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('precision').setAttribute('value', data.toString());
+    })
+    .catch(handleFetchError);
+}
+
+// Call functions to update accuracy and precision values
+document.addEventListener('DOMContentLoaded', function() {
+  updateAccuracy();
+  updatePrecision();
+});
+
+// Fetch data from the backend and add rows to the table
+fetch('http://localhost:8080/spamDetector-1.0/api/spam')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(testFile => {
+      addRow(testFile);
+    });
+  })
+  .catch(handleFetchError);
--- spamDetectorServer/src/main/java/com/spamdetector/service/SpamResource.java
+++ spamDetectorServer/src/main/java/com/spamdetector/service/SpamResource.java
@@ -14,51 +14,54 @@ import jakarta.ws.rs.core.Response;
 @Path("/spam")
 public class SpamResource {
 
-//    your SpamDetector Class responsible for all the SpamDetecting logic
-    SpamDetector detector = new SpamDetector();
-
+    private final SpamDetector detector = new SpamDetector();
 
     SpamResource(){
-//        TODO: load resources, train and test to improve performance on the endpoint calls
+//      TODO: load resources, train and test to improve performance on the endpoint calls
         System.out.print("Training and testing the model, please wait");
-
-//      TODO: call  this.trainAndTest();
-
-
+        this.trainAndTest();
     }
+
     @GET
     @Produces("application/json")
     public Response getSpamResults() {
-//       TODO: return the test results list of TestFile, return in a Response object
-
-        return null;
+        List<TestFile> testResults = trainAndTest();
+        Response response = Response.status(200)
+                .header("Access-Control-Allow-Origin", "http://localhost:63342")
+                .header("Content-Type", "application/json")
+                .entity(testResults.toArray(new TestFile[0]))
+                .build();
+        return response;
     }
-
     @GET
     @Path("/accuracy")
     @Produces("application/json")
     public Response getAccuracy() {
-//      TODO: return the accuracy of the detector, return in a Response object
-
-        return null;
+        double accuracy = detector.getAccuracy();
+        Response response = Response.status(200)
+                .header("Access-Control-Allow-Origin", "http://localhost:63342")
+                .header("Content-Type", "application/json")
+                .entity(accuracy)
+                .build();
+        return response;
     }
 
     @GET
     @Path("/precision")
     @Produces("application/json")
     public Response getPrecision() {
-       //      TODO: return the precision of the detector, return in a Response object
-
-        return null;
+        double precision = detector.getPrecision();
+        Response response = Response.status(200)
+                .header("Access-Control-Allow-Origin", "http://localhost:63342")
+                .header("Content-Type", "application/json")
+                .entity(precision)
+                .build();
+        return response;
     }
 
-    private List<TestFile> trainAndTest()  {
-        if (this.detector==null){
-            this.detector = new SpamDetector();
-        }
+    private List<TestFile> trainAndTest() {
 
-//        TODO: load the main directory "data" here from the Resources folder
-        File mainDirectory = null;
-        return this.detector.trainAndTest(mainDirectory);
+        File mainDirectory = new File(getClass().getClassLoader().getResource("data").getFile());
+        return detector.trainAndTest(mainDirectory);
     }
 }
