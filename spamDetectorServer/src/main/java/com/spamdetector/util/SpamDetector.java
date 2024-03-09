4c70e1efa91e3552affb1827e2e2e875e6dc46c7
sabaoonmj <sabaoon.mohammadjamil@ontariotechu.net>
2024-03-09 14:07:16 -0500

    detector updated

--- spamDetectorServer/src/main/java/com/spamdetector/util/SpamDetector.java
+++ spamDetectorServer/src/main/java/com/spamdetector/util/SpamDetector.java
@@ -12,59 +12,101 @@ import java.util.*;
 public class SpamDetector {
     private Map<String, Integer> trainHamFreq;
     private Map<String, Integer> trainSpamFreq;
+    private Map<String, Double> prSwiMap;
     private List<TestFile> testResults;
+    private int totalHamFiles;
+    private int totalSpamFiles;
+
     public List<TestFile> trainAndTest(File mainDirectory) {
-        // main method of loading the directories and files, training and testing the model
         trainHamFreq = new HashMap<>();
         trainSpamFreq = new HashMap<>();
+        prSwiMap = new HashMap<>();
         testResults = new ArrayList<>();
+        totalHamFiles = 0;
+        totalSpamFiles = 0;
 
-        // Training phase
-        trainModel(new File(mainDirectory, "train/ham"), trainHamFreq);
-        trainModel(new File(mainDirectory, "train/ham2"), trainHamFreq);
-        trainModel(new File(mainDirectory, "train/spam"), trainSpamFreq);
+        //We traverse the ham and spam Files and get the trainHamFreq and trainSpamFreq
+        //trainHamFreq contains a map of words and the number of files containing that word in the ham folder
+        //trainSpamFreq contains a map of words and the number of files containing that word in the spam folder
+        trainModel(new File(mainDirectory, "train/ham"), trainHamFreq, false);
+        trainModel(new File(mainDirectory, "train/ham2"), trainHamFreq, false);
+        trainModel(new File(mainDirectory, "train/spam"), trainSpamFreq, true);
 
-        // Testing phase
+        //Calc Pr(S|Wi)
+        calculatePrSwi();
+
+        //Testing
         testResults.addAll(testModel(new File(mainDirectory, "test/ham"), false));
         testResults.addAll(testModel(new File(mainDirectory, "test/spam"), true));
 
         return testResults;
     }
 
-    private void trainModel(File directory, Map<String, Integer> freqMap) {
+    private void trainModel(File directory, Map<String, Integer> freqMap, boolean isSpam) {
         File[] files = directory.listFiles();
+        //check if files not empty
         if (files != null) {
+            Set<String> wordsInCurrentFile = new HashSet<>();
             for (File file : files) {
-                Set<String> words = extractWords(file);
-                for (String word : words) {
+                wordsInCurrentFile.clear(); //Clear set
+                extractWords(file, wordsInCurrentFile);
+                for (String word : wordsInCurrentFile) {
                     freqMap.put(word, freqMap.getOrDefault(word, 0) + 1);
                 }
             }
+            //Update number files
+            if (isSpam) {
+                totalSpamFiles += 1;
+            } else {
+                totalHamFiles += 1;
+            }
         }
+    }
+
+    private void calculatePrSwi() {
+        prSwiMap = new HashMap<>();
+        Set<String> allWords = new HashSet<>();
+        allWords.addAll(trainHamFreq.keySet());
+        allWords.addAll(trainSpamFreq.keySet());
+        //iterate over all the words and find prWi|S and prWi|H
+        for (String word : allWords) {
+            double prWiGivenSpam = calculatePrWiGivenClass(word, trainSpamFreq, totalSpamFiles);
+            double prWiGivenHam = calculatePrWiGivenClass(word, trainHamFreq, totalHamFiles);
+               //given both probs, we find prSwi
+            double prSwi = prWiGivenSpam / (prWiGivenSpam + prWiGivenHam);
+            prSwiMap.put(word, prSwi);
+        }
+    }
+
+    //this is calculation for prWi|S and prWi|H
+    private double calculatePrWiGivenClass(String word, Map<String, Integer> freqMap, int totalFiles) {
+        int filesContainingWord = freqMap.getOrDefault(word, 0);
+        return (double) filesContainingWord / totalFiles;
     }
 
     private List<TestFile> testModel(File directory, boolean expectedSpam) {
         List<TestFile> testFiles = new ArrayList<>();
         File[] files = directory.listFiles();
+        //files not empty
         if (files != null) {
             for (File file : files) {
-                Set<String> words = extractWords(file);
+                Set<String> words = new HashSet<>();
+                //get all the words from file and put in set
+                extractWords(file, words);
+                //use given set of words to calc spamProb of file
                 double spamProbability = calculateSpamProbability(words);
-                // Set the actual class based on the expectedSpam parameter
                 String actualClass = expectedSpam ? "spam" : "ham";
                 testFiles.add(new TestFile(file.getName(), spamProbability, actualClass));
             }
         }
         return testFiles;
     }
-
-    private Set<String> extractWords(File file) {
-        Set<String> words = new HashSet<>();
+//extracts all the words
+    private void extractWords(File file, Set<String> words) {
         try (Scanner scanner = new Scanner(file)) {
             while (scanner.hasNext()) {
                 String word = scanner.next().toLowerCase();
-                // Simple word extraction (can be improved)
-                word = word.replaceAll("[^a-zA-Z]", ""); // Remove non-alphabetic characters
+                word = word.replaceAll("[^a-zA-Z]", "");
                 if (!word.isEmpty()) {
                     words.add(word);
                 }
@@ -72,70 +114,77 @@ public class SpamDetector {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        return words;
     }
 
     private double calculateSpamProbability(Set<String> words) {
-        double spamLogProbability = 0.0;
-        double hamLogProbability = 0.0;
+        double eta = 0.0;
 
         for (String word : words) {
-            int hamCount = trainHamFreq.getOrDefault(word, 0);
-            int spamCount = trainSpamFreq.getOrDefault(word, 0);
+            double prSwi = prSwiMap.getOrDefault(word, 1.0 / (totalSpamFiles + 2));
 
-            // Calculate Pr(W|S) and Pr(W|H)
-            double pWS = (spamCount + 1.0) / (spamCount + hamCount + 2.0);
-            double pWH = (hamCount + 1.0) / (spamCount + hamCount + 2.0);
+            if (Double.isNaN(prSwi) || Double.isInfinite(prSwi)) {
+                continue;
+            }
 
-            // Convert to log probabilities and add to the total
-            spamLogProbability += Math.log(pWS);
-            hamLogProbability += Math.log(pWH);
+            //This part is to deal with log(0) or log(1)
+            //log(0) is undefined and causes issues
+            //as well as log(1) which is 0. It also causes errors
+            //Change the values to something very near to 1 or 0
+            if (prSwi == 0) {
+                prSwi = 0.000000001;
+            } else if (prSwi == 1) {
+                prSwi = 0.999999999;
+            }
+
+            //Calculate the eta
+            eta += Math.log(1 - prSwi) - Math.log(prSwi);
         }
 
-        // Convert back to regular probabilities
-
-        return 1.0 / (1.0 + Math.exp(hamLogProbability - spamLogProbability));
+        //Calculate spamProbability
+        double probability = 1 / (1 + Math.exp(eta));
+        //Return probability
+        return probability;
     }
 
     public double getAccuracy() {
-        int numTruePositive = 0;
-        int numTrueNegative = 0;
+        double numTruePositive = 0;
+        double numTrueNegative = 0;
 
-        // Iterate over the test files
+        //Iterate test files
         for (TestFile testFile : testResults) {
-            // Check if the actual class is 'spam' and predicted class is also 'spam'
+            // Check actual class = 'spam' & check spam probability > 0.5
             if (testFile.getActualClass().equals("spam") && testFile.getSpamProbability() > 0.5) {
-                numTruePositive++; // Increment true positive count
+                numTruePositive++;
             }
-            // Check if the actual class is 'ham' and predicted class is also 'ham'
+            // Check actual class is 'ham' & check spam probability <= 0.5
             else if (testFile.getActualClass().equals("ham") && testFile.getSpamProbability() <= 0.5) {
-                numTrueNegative++; // Increment true negative count
+                numTrueNegative++;
             }
         }
 
         // Calculate accuracy
         int totalFiles = testResults.size();
-        return (double) (numTruePositive + numTrueNegative) / totalFiles;
+        return (numTruePositive + numTrueNegative) / totalFiles;
     }
 
     public double getPrecision() {
-        int numTruePositive = 0;
-        int numFalsePositive = 0;
+        double numTruePositive = 0;
+        double numFalsePositive = 0;
 
-        // Iterate over the test files
+        //Iterate test files
         for (TestFile testFile : testResults) {
-            // Check if the actual class is 'spam' and predicted class is also 'spam'
+            //Check actual class = 'spam' & check spam probability > 0.5
             if (testFile.getActualClass().equals("spam") && testFile.getSpamProbability() > 0.5) {
-                numTruePositive++; // Increment true positive count
+                numTruePositive++;
             }
-            // Check if the actual class is 'ham' but predicted class is 'spam'
+            //Check actual class = 'ham' & check spam probability > 0.5
             else if (testFile.getActualClass().equals("ham") && testFile.getSpamProbability() > 0.5) {
-                numFalsePositive++; // Increment false positive count
+                numFalsePositive++;
             }
         }
 
         // Calculate precision
-        return (double) numTruePositive / (numTruePositive + numFalsePositive);
+        return numTruePositive / (numTruePositive + numFalsePositive);
     }
 
 }
